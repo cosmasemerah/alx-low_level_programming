b4ee41fc77244873c5b0ea533a34ef041578f966
costech95 <emerahcosmas@gmail.com>
2022-10-14 02:42:46 +0100

    modified file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+  * main - Entry point of the program
+  * Return: 0 if there is no error, non-zero otherwise
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
