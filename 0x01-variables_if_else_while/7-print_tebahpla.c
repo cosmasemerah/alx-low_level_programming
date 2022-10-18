dd8a007b6f0020bd128936b7188d968983847051
costech95 <emerahcosmas@gmail.com>
2022-10-18 02:14:35 +0100

    added file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - entrance point the program
+  * Return: zero if no error and non-zero if error is encountered
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
