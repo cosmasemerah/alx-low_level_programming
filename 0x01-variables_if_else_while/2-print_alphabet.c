144e63716a54f2ada7a7ecfd1e00d4e1bda64f58
costech95 <emerahcosmas@gmail.com>
2022-10-18 01:24:06 +0100

    added file 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - entrance point the program
+  * Return: zero if no error and non-zero if error is encountered
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch"\n");
+	}
+	return (0);
+}
