b384b5e36dff2856387b5285047cb6a25a787c08
costech95 <emerahcosmas@gmail.com>
2022-10-18 01:38:18 +0100

    added file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - entrance point the program
+  * Return: zero if no error and non-zero if error is encountered
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
