40ca61e58675a66ff88e52e60fb62e98485ba636
costech95 <emerahcosmas@gmail.com>
2022-10-22 22:17:59 +0100

    added file 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - finds and prints the first 98 fibonacci numbers
+  * staring with 1 and 2
+  * return: Always 0
+  */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 1, fib2 = 2, sum;
+
+	for (count = 1; count <= 98; count++)
+	{
+		sum = fib1 + fib2;
+
+		printf("%lu ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
