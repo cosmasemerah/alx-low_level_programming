ea2e4028f7ed88768e99fcb8187f8524f90e398c
costech95 <emerahcosmas@gmail.com>
2022-11-07 22:09:32 +0100

    added file 4-strpbrk.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j;
+	int count = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++;
+			}
+		}
+		else
+		return (count);
+	}
+	return (count);
 }
