80d7e03f51027fee8dd1f362a7cf09e1b9612882
costech95 <emerahcosmas@gmail.com>
2022-11-21 14:37:17 +0100

    modified files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,10 +18,10 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	while(s1 && s1[len1])
+	while (s1 && s1[len1])
 		len1++;
 
-	while(s2 && s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1)
 	{
-		while(i < len1)
+		while (i < len1)
 		{
 			s3[i] = s1[i];
 			i++;
@@ -41,7 +41,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2)
 	{
-		while(i < (len1 + len2))
+		while (i < (len1 + len2))
 		{
 			s3[i] = s2[j];
 			i++;
